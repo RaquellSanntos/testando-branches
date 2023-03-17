cec28120fcd8662b6b7c45d4b9e65b9ea7b9f168
RaquelSFoursys <raquel.soares@foursys.com.br>
2023-03-16 23:40:37 -0400

    enviando primeiro commit

---
+++ src/CompartilharGitTeams.java
@@ -0,0 +1,10 @@
+
+public class CompartilharGitTeams {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Olá, eu sou a Branche Master "
+				+ "do projeto 'Testando Branches!");
+	}
+
+}
